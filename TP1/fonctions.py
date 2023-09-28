842b0ea61620c6676982d8038447deb18035bbf7
RTLERATIO <rtleratio@gmail.com>
2023-09-28 15:16:04 +0200

    Fin du TP1

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,11 +1,17 @@
 def puissance(a,b):
-
+	p=1
 	if not type(a) and type(b) is int :
 		raise TypeError("Only integers are allowed")
-	return a**b
-
+		
 	if a == 0 and b == 0 :
-		return 1
-
-	if 
-
+		return 1	
+		
+	if a < 0 and b == 0:
+		return -1
+			
+	if a == 0 and b < 0:
+		raise ValueError("B ne doit pas être une valeur negative")
+		
+	for i in range(b):
+		p=p*a
+	return p 
--- TP1/test.py
+++ TP1/test.py
@@ -9,10 +9,12 @@ def test_2():
 	assert f.puissance(-2,3) == -8
 	assert f.puissance(-2,2) == 4
 def test_3():
-	assert f.puissance(2,2) == 4
-	assert f.puissance(-2,2) == 4
-	assert f.puissance(2,0) == 1
-	assert f.puissance(-2,0) == -1
-	assert f.puissance(0,0) == 1
-	assert f.puissance(0,3) == 0
-	assert f.puissance(0,-3) == 0
+	with pytest.raises(ValueError, match="B ne doit pas être une valeur negative"):
+		assert f.puissance(2,2) == 4
+		assert f.puissance(-2,2) == 4
+		assert f.puissance(2,0) == 1
+		assert f.puissance(-2,0) == -1
+		assert f.puissance(0,0) == 1
+		assert f.puissance(0,3) == 0
+		assert f.puissance(0,-3) == 0
+	
