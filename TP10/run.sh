33bcac683c1b85f20f1a4d69a2d1ccfb06329f06
user <rtleratio>
2024-02-02 13:56:56 +0100

    ajout de fichier

--- TP10/run.sh
+++ TP10/run.sh
@@ -1,1 +1,13 @@
-java -classpath "/home/user/lib/*.java" $1
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "Erreur: Aucun argument fourni."
+    exit 1
+fi
+
+if [ "$1" == "" ]; then
+    echo "Erreur: Argument vide."
+    exit 1
+else
+    java -classpath "/home/user/lib/*.java" "$1"
+fi
